31ef2d27f2aaaf81c44ffbd23630803971acde82
anaforttes <carolfortes05@gmail.com>
2023-05-26 13:55:35 -0300

    Initial commit

---
+++ Desenvolvedor.java
@@ -0,0 +1,28 @@
+public class Desenvolvedor extends Funcionario {
+    private String linguagem;
+    private String nivel;
+
+    public Desenvolvedor(String nome, double salario, String linguagem) {
+        super();
+        this.linguagem = linguagem;
+        this.nivel = "Junior"; // Define o nível inicial como Júnior
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void promoverParaPleno() {
+        nivel = "Pleno";
+        // Adicione qualquer comportamento adicional para a promoção para Pleno
+    }
+
+    public void promoverParaSenior() {
+        nivel = "Senior";
+        // Adicione qualquer comportamento adicional para a promoção para Sênior
+    }
+}
---
+++ Funcionario.java
@@ -0,0 +1,65 @@
+public abstract class Funcionario {
+    private String nome;
+    private String email;
+    private int cpf;
+    private int idade;
+    private String telefone;
+    private double salario;
+
+    public Funcionario() {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void solicitarFerias() {
+        System.out.println("Funcionário " + getNome() + " solicitou férias!!");
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
